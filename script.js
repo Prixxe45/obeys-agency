03876c2f94ed5508867c38479395dc87b7e3fcc8
Prince Tripathi <prince45tri@gmail.com>
2025-03-12 00:21:40 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var tl = gsap.timeline();
-
-
+function loadingAnimation( ){
+  var tl = gsap.timeline();
 
 tl.from(".line h1",{
   y:150,
@@ -20,24 +19,46 @@ clearInterval(int);
  grow++;
 h5.innerHTML = grow;
 },30);
-  } 
+  }
 });
-tl.to(".line h2",{
+tl.to('.line h2',{
   animationName:"anime",
-  opacity:1,
+  opacity:1
 })
 tl.to("#loader",{
   opacity:0,
   duration:0.2,
-  delay:3.3,
+  delay:0,
 });
-tl.to("#page1",{
+tl.from("#page1",{
+  y:1600,
   delay:0.2,
-  y:1200,
-  duration:0.5,
   opacity:0,
+  duration:0.5,
   ease:Power4,
 })
 tl.to("#loader",{
   display:"none",
 })
+tl.from("#nav",{
+  opacity:0,
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1 ",{
+  y:120,
+  stagger:0.2
+})
+
+}
+loadingAnimation();
+
+function cursorAnimation() {
+  document.addEventListener("mousemove",function(dets){
+gsap.to("#crsr",{
+  top:dets.y,
+  left:dets.x,
+})
+})
+Shery.makeMagnet("#nav-part2 h4", {
+});
+}
+cursorAnimation();
